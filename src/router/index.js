0a89e324e2c2e8441771a85ad7b1a13032e1a69a
samnangnouy <samnangnouy200@gmail.com>
2023-06-28 23:13:18 +0700

    Add Product Detail Page

--- src/router/index.js
+++ src/router/index.js
@@ -24,15 +24,15 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/Products.vue')
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ProductDetail.vue')
     }
-    // {
-    //   path: '/home',
-    //   name: 'Home',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/home.vue')
-    // }
   ]
 })
 
