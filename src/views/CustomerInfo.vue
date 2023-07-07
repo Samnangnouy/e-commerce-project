635e228b43a0aedf8538061627134f19465c98b4
samnangnouy <samnangnouy200@gmail.com>
2023-07-07 23:16:56 +0700

    Modified on customer information page

---
+++ src/views/CustomerInfo.vue
@@ -0,0 +1,249 @@
+<template>
+    <div class="head">
+      <div class="title">
+        <h3>Hero Boots</h3>
+      </div>
+      <div class="content">
+        <a href="#">Home |</a>
+        <a href="#">About |</a>
+        <a href="#">Contact |</a>
+      </div>
+      <div class="search">
+        <div class="icon">
+          <img src="@/assets/Images/search.png" />
+        </div>
+        <input type="text" placeholder="Search here" />
+        <div class="icon1">
+          <img src="@/assets/Images/shopping-cart.png" />
+        </div>
+      </div>
+    </div>
+    <div class="relate-product">
+      <h1>Your Information</h1>
+      <p>Please spend few minute to complete your information here</p>
+    </div>
+    <div class="customer">
+        <div class="customer-info">
+            <div class="email">
+                <h2>Customer Information</h2>
+                <div class="title">
+                    <h2>Already have an account</h2>
+                    <a href="">Login</a>
+                </div>
+            </div>
+            <input type="text" placeholder="Your email">
+        </div>
+        <div class="summary">
+
+        </div>
+    </div>
+    <div class="footer">
+      <div class="followup">
+        <p>Follow Us On</p>
+        <img src="@/assets/Images/tik-tok.png" />
+        <img src="@/assets/Images/instagram.png" />
+        <img src="@/assets/Images/facebook.png" />
+        <img src="@/assets/Images/twitter.png" />
+      </div>
+      <div class="subscribe">
+        <p>Stay in Touch! Join Our Newletters</p>
+        <input type="text" placeholder="Enter your email" />
+        <button>subscribe</button>
+      </div>
+    </div>
+  </template>
+  
+  <style>
+  *{
+      font-family: 'Inria Serif';
+    }
+    .head{
+      height: 120px;
+      background-color: #4391AA;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding-left: 80px;
+      padding-right: 80px;
+    }
+    .title h3{
+      font-size: 26px;
+      color: #fff;
+    }
+    .content a{
+      margin: 10px;
+      font-size: 22px;
+      color: #fff;
+      display: inline-block;
+      text-decoration: none;
+      font-family: Inria Serif;
+    }
+    .content a::after{
+      content: '';
+      width: 0%;
+      height: 2px;
+      display: block;
+      background-color: #FFFFFF;
+      transition: 0.5s;
+    }
+    .content a:hover::after{
+      width: 100%;
+    }
+    .search{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .search input{
+      width: 224px;
+      height: 38px;
+      border-top-right-radius: 10px;
+      border-bottom-right-radius: 10px;
+      font-size: 20px;
+      border: none;
+      outline: none;
+      padding-left: 15px;
+      width: 300px;
+      background-color: #D9D9D9;
+    }
+    .search input::placeholder{
+      opacity: 0.5;
+      color: #D18585;
+    }
+    .search .icon{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 50px;
+      height: 40px;
+      background-color: #D9D9D9;
+      border-top-left-radius: 10px;
+      border-bottom-left-radius: 10px;
+    }
+    .search .icon1{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 50px;
+      height: 40px;
+    }
+    .search .icon1 img{
+      width: 30px;
+      height: 30px;
+      cursor: pointer;
+    }
+    .search .icon img{
+      width: 30px;
+      height: 30px;
+      border-top-left-radius: 10px;
+      border-bottom-left-radius: 10px;
+      color: transparent;
+    }
+    .relate-product{
+    height: 104px;
+    background-color: #BC95AD;
+    margin-top: 15px;
+    margin-left: 10px;
+    margin-right: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .relate-product h1{
+    margin-top: 0;
+    font-size: 32px;
+    margin-bottom: 10px;
+  }
+  .relate-product p{
+    font-size: 18px;
+    color: #FFFFFF;
+    margin-top: 0;
+  }
+  .customer{
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 10px;
+    background-color: #BC95AD;
+  }
+  .customer .customer-info{
+    width: 55%;
+    background-color: blue;
+    border: 1px solid black;
+    border-left: none;
+    border-right: none;
+  }
+  .customer .customer-info .email{
+    display: flex;
+    align-items: baseline;
+    justify-content: space-between;
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+  .customer .customer-info .email .title{
+    display: flex;
+    align-items: baseline;
+  }
+  .customer .customer-info input{
+    margin-left: 10px;
+    margin-right: 10px;
+    width: 94%;
+    height: 42px;
+    font-size: 20px;
+    padding-left: 20px;
+    outline: none;
+  }
+    .followup{
+      display: flex;
+      align-items: center;
+      margin-left: 70px;
+    }
+    .followup p{
+      font-size: 20px;
+      font-weight: 600;
+      margin-right: 15px;
+    }
+    .followup img{
+      width: 30px;
+      height: 30px;
+      margin-right: 15px;
+      cursor: pointer;
+    }
+    .subscribe{
+      margin-right: 70px;
+    }
+    .subscribe p{
+      font-size: 16px;
+      font-weight: 600;
+      margin-bottom: 10px;
+    }
+    .subscribe input{
+      width: 225px;
+      height: 45px;
+      padding-left: 60px;
+      font-size: 16px;
+      font-weight: 600;
+      color: #222;
+      outline: none;
+    }
+    .subscribe input::placeholder{
+      color: #222;
+    }
+    .subscribe button{
+      width: 122px;
+      height: 50px;
+      font-size: 16px;
+      font-weight: 600;
+      color: #FFFFFF;
+      background-color: #4B43AA;
+      border: none;
+      cursor: pointer;
+    }
+    .subscribe button:hover{
+      background-image: linear-gradient(to right, #f83600 0%, #f9d423 100%);
+      box-shadow: 5px 5px 10px 2px rgba(0,0,0,.8);
+      transition-duration: 0.4s;
+    }
+  </style>
+  
