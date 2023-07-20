c82bac83fe9ab24c9a3d8c73eabca556e0ddc90c
samnangnouy <samnangnouy200@gmail.com>
2023-07-20 14:12:53 +0700

    Finish Customer info page

--- src/views/CustomerInfo.vue
+++ src/views/CustomerInfo.vue
@@ -27,16 +27,60 @@
             <div class="email">
                 <h2>Customer Information</h2>
                 <div class="title">
-                    <h2>Already have an account</h2>
+                    <h2>Already have an account?</h2>
                     <a href="">Login</a>
                 </div>
             </div>
-            <input type="text" placeholder="Your email">
+            <input class="input1" type="text" placeholder="Your email">
+            <div class="shipping">
+              <h1>Shipping Address</h1>
+              <div class="name">
+                <input type="text" placeholder="First Name">
+                <input type="text" placeholder="Last Name">
+              </div>
+              <input class="input1" type="text" placeholder="Company">
+              <div class="add">
+                <input type="text" placeholder="Address">
+                <input type="text" placeholder="Apt">
+              </div>
+              <div class="coun">
+                <input type="text" placeholder="Country">
+                <input type="text" placeholder="State">
+                <input type="text" placeholder="Zip">
+              </div>
+            </div> 
         </div>
         <div class="summary">
-
+          <h1>Summary(1 item)</h1>
+          <table>
+          <tr>
+            <td>Subtotal</td>
+            <td>$20.00</td>
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>Shipping</td>
+            <td>-- --</td>
+          </tr>
+        </table>
+        <table>
+          <tr>
+            <td>Est. Taxes</td>
+            <td>-- --</td>
+          </tr>
+        </table>
+        <div class="gift">
+          <h1>Gift Card or discount code</h1>
+          <div class="btn">
+          <input type="text">
+          <button>Apply</button>
+        </div>
+        </div>
         </div>
     </div>
+    <button class="return">Return to cart</button>
+    <button class="continue">Continue to Shipping Method</button>
     <div class="footer">
       <div class="followup">
         <p>Follow Us On</p>
@@ -164,15 +208,12 @@
     margin-left: 10px;
     margin-right: 10px;
     margin-top: 10px;
-    padding: 10px;
-    background-color: #BC95AD;
+    display: flex;
   }
   .customer .customer-info{
     width: 55%;
-    background-color: blue;
-    border: 1px solid black;
-    border-left: none;
-    border-right: none;
+    background-color: #BC95AD;
+    margin-right: 10px;
   }
   .customer .customer-info .email{
     display: flex;
@@ -185,15 +226,130 @@
     display: flex;
     align-items: baseline;
   }
-  .customer .customer-info input{
+  .customer .customer-info .input1{
     margin-left: 10px;
     margin-right: 10px;
     width: 94%;
-    height: 42px;
+    height: 30px;
     font-size: 20px;
     padding-left: 20px;
     outline: none;
   }
+  .shipping h1{
+    margin-left: 10px;
+  }
+  .name,
+  .add,
+  .coun{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+  .name input{
+    width: 45%;
+    height: 30px;
+    padding-left: 10px;
+    outline: none;
+    margin-bottom: 10px;
+  }
+  .add input:nth-child(1){
+    width: 60%;
+    height: 30px;
+    padding-left: 10px;
+    outline: none;
+    margin-bottom: 10px;
+    margin-top: 10px;
+    margin-left: 3px;
+  }
+  .add input:nth-child(2){
+    width: 30%;
+    height: 30px;
+    padding-left: 10px;
+    outline: none;
+    margin-bottom: 10px;
+    margin-top: 10px;
+    margin-left: 7px;
+  }
+  .coun input{
+    width: 200px;
+    height: 30px;
+    outline: none;
+    padding-left: 10px;
+    margin-bottom: 10px;
+  }
+  .summary{
+    padding: 10px;
+    width: 45%;
+    background-color: #BC95AD;
+    margin-left: 10px;
+    height: auto;
+  }
+  .summary table{
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+  margin-bottom: 6px;
+}
+.summary table td{
+  border: 1px solid black;
+  width: 100px;
+  text-align: center;
+  background-color: #ffffff;
+  padding: 8px;
+}
+.gift{
+  margin-top: 10px;
+  border: 1px solid black;
+  border-left: none;
+  border-right: none;
+}
+.gift h1{
+  margin-bottom: 10px;
+}
+.btn{
+  width: 100%;
+}
+.btn input{
+  width: 200px;
+  height: 30px;
+  outline: none;
+  font-size: 18px;
+}
+.btn button{
+  width: 100px;
+  height: 37px;
+  background-color: rgb(168, 119, 214);
+  border: none;
+  font-size: 18px;
+  color: #ffffff;
+  margin-left: 10px;
+  margin-top: 10px;
+  cursor: pointer;
+  margin-bottom: 10px;
+}
+.return{
+  width: 200px;
+  height: 45px;
+  background-color: rgb(168, 119, 214);
+  border: none;
+  font-size: 18px;
+  color: #ffffff;
+  margin-left: 10px;
+  margin-top: 10px;
+  cursor: pointer;
+}
+.continue{
+  width: 300px;
+  height: 45px;
+  background-color: rgb(168, 119, 214);
+  border: none;
+  font-size: 18px;
+  color: #ffffff;
+  margin-left: 40px;
+  margin-top: 10px;
+  cursor: pointer;
+}
     .followup{
       display: flex;
       align-items: center;
